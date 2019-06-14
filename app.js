f95365bea1d7e84ff986c797f9982657e51cd2a2
Ryan Abraham <rabraham@uaudio.com>
2019-06-14 13:23:38 -0700

    Use ES2018 syntax in app.js

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 // including libraries and api config
-var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    fs = require('fs'),
-    twit = require('twit'),
-    config = require('./config'),
-    wordBank = require('./public/js/wordbank.json'),
-    endpoint = 'statuses/sample',
-    static = require('node-static'), // for serving files
-    tweetCount = 0,
-    currentMood = '',
-    now, start = new Date(),
-    tps = 0; // tweets per second
+const app = require('http').createServer(handler);
+const io = require('socket.io').listen(app);
+const fs = require('fs');
+const twit = require('twit');
+const config = require('./config');
+const wordBank = require('./public/js/wordbank.json');
+const endpoint = 'statuses/sample';
+const nodeStatic = require('node-static'); // for serving files
+// this will make the public folder accessible from the web
+const fileServer = new nodeStatic.Server('./public');
+// instantiate twitter client
+const twitter = new twit(config);
+// subscribe to the stream of tweets from desired endpoint
+const stream = twitter.stream(endpoint);
 
-// to add more emotions, insert another key in wordbank.json
-// add another key to emtion tally
-// and a condition in determineMood()
-
-var emotionTally = {
-    happy: 0,
-    romantic: 0,
-    sad: 0,
-    angry: 0,
-    afraid: 0
-}
-
-// this will make all the files in the public folder accessible from the web
-var fileServer = new static.Server('./public');
-
-// if the URL of the socket server is opened in a browser
+// fulfill client requests
 function handler (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
@@ -39,52 +26,44 @@ app.listen(9000, function(){
     console.log('Listening on port 9000...');
 });
 
-// instantiate twitter client
-var twitter = new twit(config);
+// app variables
+// to add more emotions, insert another key in wordbank.json
+// add another key to emtion tally
+// and a condition in determineMood()
 
-// subscribe to the stream of tweets from desired endpoint
-var stream = twitter.stream(endpoint);
+let tweetCount = 0;
+let currentMood = '';
+let start = new Date();
+let tps = 0; // tweets per second
+let now;
+let emotionMap = {
+    happy: 0,
+    romantic: 0,
+    sad: 0,
+    angry: 0,
+    afraid: 0
+};
 
-// listen for stream connection
-stream.on('connect', (params) => {
-    console.log('Streaming from the Twitter API...');
-});
-
-// listen for tweets
-stream.on('tweet', (tweet) => {
-    tweetCount++;
-    if (tweet.lang === 'en') {
-        determineMood(tweet);
-    } else {
-        return;
+const determineMood = (tweet) => {
+    const containsWord = (word) => {
+        return tweet.text.toLowerCase().indexOf(word) !== -1;
     }
-});
 
-// listen for errors
-stream.on('error', (err) => {
-    console.log('error!', err);
-});
-
-// determine mood
-function determineMood(tweet) {
-    if (wordBank.happyWords.some((v) => { return tweet.text.toLowerCase().indexOf(v) !== -1; })) {
-        emotionTally.happy++;
-    } else if (wordBank.romanticWords.some((v) => { return tweet.text.toLowerCase().indexOf(v) !== -1; })) {
-        emotionTally.romantic++;
-    } else if (wordBank.sadWords.some((v) => { return tweet.text.toLowerCase().indexOf(v) !== -1; })) {
-        emotionTally.sad++;
-    } else if (wordBank.angryWords.some((v) => { return tweet.text.toLowerCase().indexOf(v) !== -1; })) {
-        emotionTally.angry++;
-    } else if (wordBank.afraidWords.some((v) => { return tweet.text.toLowerCase().indexOf(v) !== -1; })) {
-        emotionTally.afraid++;
-    } else {
-        return;
+    if (wordBank.happyWords.some(containsWord)) {
+        emotionMap.happy += 1;
+    } else if (wordBank.romanticWords.some(containsWord)) {
+        emotionMap.romantic += 1;
+    } else if (wordBank.sadWords.some(containsWord)) {
+        emotionMap.sad += 1;
+    } else if (wordBank.angryWords.some(containsWord)) {
+        emotionMap.angry += 1;
+    } else if (wordBank.afraidWords.some(containsWord)) {
+        emotionMap.afraid += 1;
     }
 }
 
-// reset emotion tally
-function resetEmotionTally() {
-    emotionTally = {
+const resetEmotionMap = () => {
+    emotionMap = {
         happy: 0,
         romantic: 0,
         sad: 0,
@@ -94,33 +73,51 @@ function resetEmotionTally() {
 }
 
 // alert clients of mood change
-function changeMood(emotion) {
-    // console.log('mood change!');
+const changeMood = (emotion) => {
     io.sockets.emit('moodChange', emotion);
 }
 
-// calculate tweets per second
-setInterval(function() {
+// calculate tweets per second every 5s
+setInterval(() => {
     now = new Date();
-    newTps = parseInt(1000 * tweetCount / (now - start), 10);
+    const newTps = parseInt(1000 * (tweetCount / (now - start)), 10);
     // only alert clients if there is a change
     if (newTps !== tps) {
         io.sockets.emit('tps', newTps);
         tps = newTps;
     }
-}, 1000);
+}, 5000);
 
-// check for mood change
-setInterval(function() {
-    // console.log(Object.keys(emotionTally).map(function ( key ) { return emotionTally[key]; }));
-    var newMood = Object.keys(emotionTally).reduce((a, b) => { return emotionTally[a] > emotionTally[b] ? a : b });
+// check for mood change every 30s
+setInterval(() => {
+    const newMood = Object.keys(emotionMap).reduce((a, b) => {
+        return emotionMap[a] > emotionMap[b] ? a : b;
+    });
     // only alert clients if there is a change
     if (newMood !== currentMood) {
         changeMood(newMood);
         currentMood = newMood;
     }
-    resetEmotionTally();
-}, 30000)
+    resetEmotionMap();
+}, 30000);
+
+// listen for stream connection
+stream.on('connect', () => {
+    console.log('Streaming from the Twitter API...');
+});
+
+// listen for tweets
+stream.on('tweet', (tweet) => {
+    tweetCount += 1;
+    if (tweet.lang === 'en') {
+        determineMood(tweet);
+    }
+});
+
+// listen for errors
+stream.on('error', (err) => {
+    console.log('error!', err);
+});
 
 // listen for incoming connections from clients
 io.on('connection', (socket) => {
